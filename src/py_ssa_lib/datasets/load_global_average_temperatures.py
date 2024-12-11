6b138d259b01e845255f04b0701a581bdaf8f892
Konstantin Ibadullaev <konstantin.ibadullaev.post@gmail.com>
2024-12-11 15:14:20 +0100

    Update load_global_average_temperatures.py
    indents

--- src/py_ssa_lib/datasets/load_global_average_temperatures.py
+++ src/py_ssa_lib/datasets/load_global_average_temperatures.py
@@ -9,11 +9,11 @@ def load_global_average_temperatures_df(rawDS=False):
 		return pd.read_csv(filepath_or_buffer=df_csv , sep=",")
 	else:
 		df_csv = os.path.join(dir_path,"GlobalLandTemperaturesByMajorCity.csv")
-        glob_temp_df = pd.read_csv(filepath_or_buffer=df_csv, sep=",")
-        glob_temp_df = glob_temp_df.dropna(axis=0)
-        glob_temp_df.dt = pd.to_datetime(glob_temp_df.dt)
-        cols_list = glob_temp_df["dt"]
-        glob_temp_df = glob_temp_df.drop(columns=["dt"]).T
-        glob_temp_df.columns = cols_list.values
+	        glob_temp_df = pd.read_csv(filepath_or_buffer=df_csv, sep=",")
+	        glob_temp_df = glob_temp_df.dropna(axis=0)
+	        glob_temp_df.dt = pd.to_datetime(glob_temp_df.dt)
+	        cols_list = glob_temp_df["dt"]
+	        glob_temp_df = glob_temp_df.drop(columns=["dt"]).T
+	        glob_temp_df.columns = cols_list.values
                 
 		return glob_temp_df
